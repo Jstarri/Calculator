4e68946931e8052d7a4976e5385d325cc8f29174
Ahmar Jenkins <ahmarj519@gmail.com>
2022-12-21 15:48:49 -0500

    Division Remainders

--- calculator.py
+++ calculator.py
@@ -11,6 +11,9 @@ def multiply(x, y):
   
 def divide(x, y):
   return x / y
+
+def remainder(x, y):
+  return x % y
  
 def exponent(x, y):
   return x ** y
@@ -34,89 +37,89 @@ def rectangle_area(x, y):
   return x * y
 
 def triangle_area(x, y):
-  return 0.5(x * y)
+  return 0.5 * (x * y)
 
 def circle_area(x):
-  return 3.14(x **2)
+  return 3.14 * (x **2)
 
 def slope_intercept(x, y, z):
   return (x * y) + z
 
 print("Welcome to AJ's Calculator")  
 print("Select your operation.")
-print("1: Add")
-print("2: Subtract")
-print("3: Multiply")
-print("4: Divide")
-print("5: Exponent")
-print("6: Square Root")
-print("7: Cube Root")
-print("8: Natural Log")
-print("9: Base Log")
-print("10: Area of Square")
-print("11: Area of Rectangle")
-print("12: Area of Triangle")
-print("13: Area of Circle")
-print("14: Slope-Intercept Formula")
+print("A: Add")
+print("B: Subtract")
+print("C: Multiply")
+print("D: Divide")
+print("E: Exponent")
+print("F: Square Root")
+print("G: Cube Root")
+print("H: Natural Log")
+print("I: Base Log")
+print("J: Area of Square")
+print("K: Area of Rectangle")
+print("L: Area of Triangle")
+print("M: Area of Circle")
+print("N: Slope-Intercept Formula")
 
 while True:
-  choice = input("Enter choice(1/2/3/4/5/6/7/8/9/10/11/12/13/14): ")
+  choice = input("Enter choice(A/B/C/D/E/F/G/H/I/J/K/L/M/N): ")
 
   #Part for three-number calculations
-  if choice in ('14'):
+  if choice in ('N'):
     num4 = float(input("Enter your first number: "))
     num5 = float(input("Enter your second number: "))
     num6 = float(input("Enter your third number: ")) 
   
-  if choice == '14':
+  if choice == 'N':
     print("The answer to y = (", num4, "*", num5, ") +", num6, "=", slope_intercept(num4, num5, num6))
 
   #Part for two-number calculations
-  if choice in ('1', '2', '3', '4', '5', '9', '11', '12'):
+  if choice in ('A', 'B', 'C', 'D', 'E', 'I', 'K', 'L'):
     num1 = float(input("Enter your first number: "))
     num2 = float(input("Enter your second number: "))
   
-  if choice == '1':
-    print(num1, "+", num2, "=", add(num1, num2))
+  if choice == 'A':
+    print(num1, "plus", num2, "=", add(num1, num2))
       
-  elif choice == '2':
-    print(num1, "-", num2, "=", subtract(num1, num2))
+  elif choice == 'B':
+    print(num1, "minus", num2, "=", subtract(num1, num2))
       
-  elif choice == '3':
-    print(num1, "*", num2, "=", multiply(num1, num2))
+  elif choice == 'C':
+    print(num1, "times", num2, "=", multiply(num1, num2))
       
-  elif choice == '4':
-    print(num1, "/", num2, "=", divide(num1, num2))
+  elif choice == 'D':
+    print(num1, "divided by", num2, "=", divide(num1, num2), "with a remainder of", remainder(num1, num2))
     
-  elif choice == '5':
+  elif choice == 'E':
     print(num1, "to the power of", num2, "=", exponent(num1, num2))
   
-  elif choice == '9':
+  elif choice == 'I':
     print("The logarithm base", num2, "of", num1, "=", base_log(num1, num2))
   
-  elif choice == '11':
+  elif choice == 'K':
     print("The area of a rectangle with a length of", num1, "and a width of", num2, "=", rectangle_area(num1, num2))
   
-  elif choice == '12':
+  elif choice == 'L':
     print("The area of a triangle with a base length of", num1, "and a height of", num2, "=", triangle_area(num1, num2))
 
   #Part for one-number calculations
-  elif choice in ('6', '7', '8', '10', '13'):
+  elif choice in ('F', 'G', 'H', 'J', 'M'):
     num3 = float(input("Enter your number: "))
 
-  if choice == '6':
+  if choice == 'F':
     print("The square root of", num3, "is", square_root(num3))
 
-  elif choice == '7':
+  elif choice == 'G':
     print("The cube root of", num3, "is", cube_root(num3))
   
-  elif choice == '8':
+  elif choice == 'H':
     print("The natural logarithm of", num3, "=", nat_log(num3))
   
-  elif choice == '10':
+  elif choice == 'J':
     print("The area of a square with a length of", num3, "and a width of", num3, "=", square_area(num3))
   
-  elif choice == '13':
+  elif choice == 'M':
     print("The area of a circle with a radius of", num3, "=", circle_area(num3))
 
     
